7fceebf4cbea2ef81f79106b21def4caab698bc0
SHAKIL-25 <shakil.ahmed.emon8@gmail.com>
2025-08-13 15:44:11 +0600

    AI

---
+++ basic.py
@@ -0,0 +1,12 @@
+# Simple Python program
+
+# Print a message
+print("Hello, World!")
+
+# Add two numbers
+a = 5
+b = 3
+sum_result = a + b
+
+# Show the result
+print("The sum of", a, "and", b, "is", sum_result)
